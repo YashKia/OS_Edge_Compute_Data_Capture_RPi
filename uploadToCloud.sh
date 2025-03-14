b2887dfac8406e8fcc694baa3d449fd70066c650
Yashar Kiarashi <93733643+YashKia@users.noreply.github.com>
2025-03-14 10:42:06 -0400

    Update uploadToCloud.sh
    Update the code to remove the extracted features after 2 days- for testing

--- uploadToCloud.sh
+++ uploadToCloud.sh
@@ -58,5 +58,9 @@ rclone move /home/pi/OS_Edge_Compute_Data_Capture/data/geolocationToolbox ${remo
 # Delete all video-files that were recorded
 rm -rf /home/pi/OS_Edge_Compute_Data_Capture/data/Videos/*
 
+
+find /home/pi/PIR-interface/data/ -type f -mtime +2 -exec rm {} \;
+
+
 # Run the upload_complete.py GUI to display that upload is indeed complete
 sudo python3 /home/pi/OS_Edge_Compute_Data_Capture/codes/upload_complete.py
